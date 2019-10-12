027f3d398a93f4cc64490f6fe27721681645de79
Blanca Castellanos <blancal.castellanos.cbtis37@gmail.com>
2019-10-12 15:20:59 -0700

    Agregar archivos de proyecto.

---
+++ ProyectoSegundoParcial/Cuadernos.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoSegundoParcial
+{
+    /// <summary>
+    /// Lógica de interacción para Cuadernos.xaml
+    /// </summary>
+    public partial class Cuadernos : UserControl
+    {
+        public Cuadernos()
+        {
+            InitializeComponent();
+
+            txtaviso.Visibility = Visibility.Hidden;
+        }
+
+        private void BtnGuardar(object sender, RoutedEventArgs e)
+        {
+            //Condicional (dudas - checar)
+
+            if (tbCodA.Text == string.Empty ||
+                tbDescP.Text == string.Empty ||
+                tbCosto.Text == string.Empty ||
+                tbPrecipP.Text == string.Empty ||
+                tbProv.Text == string.Empty ||
+                tbCantidad.Text == string.Empty)
+            {
+                txtaviso.Visibility = Visibility.Visible;
+            }
+
+
+            else
+            {
+                grdFormularioArt.Children.Clear();
+            }
+
+        }
+
+        private void BtnCancelar(object sender, RoutedEventArgs e)
+        {
+            grdFormularioArt.Children.Clear();
+        }
+
+        private void TbCod_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtaviso.Visibility = Visibility.Hidden;
+        }
+
+        private void TbDescr_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtaviso.Visibility = Visibility.Hidden;
+        }
+
+        private void TbCosto_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtaviso.Visibility = Visibility.Hidden;
+        }
+
+        private void TbPrecio_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtaviso.Visibility = Visibility.Hidden;
+        }
+
+        private void TbCantidad_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtaviso.Visibility = Visibility.Hidden;
+        }
+        private void TbProveedor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtaviso.Visibility = Visibility.Hidden;
+        }
+
+    }
+}
---
+++ ProyectoSegundoParcial/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoSegundoParcial
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void cbPapeleria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            grdArticulos.Children.Clear();
+        }
+
+        private void BtnAgregar(object sender, RoutedEventArgs e)
+        {
+            switch (cbPapeleria.SelectedIndex)
+            {
+
+                case 0: //Libros
+                    grdArticulos.Children.Add(new Libros());
+                break;
+
+
+
+                case 1: //Cuadernos
+                    grdArticulos.Children.Add(new Cuadernos());
+                break;
+
+
+
+                case 2: //Revista
+                    grdArticulos.Children.Add(new Revista());
+                break;
+
+
+
+                case 3: //Cuentos
+                    grdArticulos.Children.Add(new Cuentos());
+                break;
+
+
+
+                case 4: //Periodicos
+                    grdArticulos.Children.Add(new Periodicos());
+                break;
+
+
+
+                case 5: //Agenda
+                    grdArticulos.Children.Add(new Agenda());
+                break;
+
+            }
+        }
+    }
+}
